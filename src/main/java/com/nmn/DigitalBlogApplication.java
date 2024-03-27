baaa938bbf2c5c13389f1a3037d6092f6a4b348d
NguyenMinhNhat82 <20510520925nhat@ou.edu.vn>
2024-03-27 09:22:35 +0700

    aa

--- src/main/java/com/nmn/DigitalBlogApplication.java
+++ src/main/java/com/nmn/DigitalBlogApplication.java
@@ -27,18 +27,5 @@ public class DigitalBlogApplication {
     public static void main(String[] args) {
         SpringApplication.run(DigitalBlogApplication.class,args);
     }
-    @Bean
-    public void setUp(){
-        UserDTO users = new UserDTO(0,"nhat","123", "SYS_ADMIN",true,true);
-        userService.addOrUpdateProfile(users);
-        for(int i = 2; i< 10;i++){
-            UserDTO userCus = new UserDTO(0,"nhat"+i,"123", "CUSTOMER_USER",true,true);
-            userService.addOrUpdateProfile(userCus);
-        }
-        for(int i = 2; i< 10;i++){
-            ArticleDTO articleDTO = new ArticleDTO(0,"title"+i,"content"+i, LocalDateTime.now(), LocalDateTime.now(),i);
-            Users users1 = userService.getUserbyId(articleDTO.getUserId());
-            articleService.saveArticle(articleDTO,users1);
-        }
-    }
+
 }
